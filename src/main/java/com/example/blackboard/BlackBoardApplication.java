a0c3042a9194f763699ee4151b535e6baaf5eb6b
th <upperrr@gmail.com>
2024-01-25 02:14:13 +0900

    Initial commit

---
+++ src/main/java/com/example/blackboard/BlackBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blackboard/BlackBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
